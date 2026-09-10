09d69f6f91b27638b46b4b9d0cdbf4dcbba3f2b2
krishnachaitanya-rokkam <krishnachaitanya.rokkam@outlook.com>
2026-09-10 14:52:17 +0530

    Start family goal investment reconciliation globally

--- main.tsx
+++ main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { startFamilyGoalInvestmentSync } from './src/services/familyGoalInvestmentSync';
+
+startFamilyGoalInvestmentSync();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
